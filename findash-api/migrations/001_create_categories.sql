e8b932c894c538c838e30be91798ec400f3f1013
AlanSarasate <alansarasatecc@gmail.com>
2026-01-12 00:01:47 -0300

    feat: Alterando primary key das migrations

--- findash-api/migrations/001_create_categories.sql
+++ findash-api/migrations/001_create_categories.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS categories (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     type VARCHAR(20) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
